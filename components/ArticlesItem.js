cd804ce6b868716a122be32aca0a1eaae12a7d5f
Kathy Tavia <kathy.tavia@gmail.com>
2021-10-21 20:57:34 +0200

    set up article cards

--- components/ArticlesItem.js
+++ components/ArticlesItem.js
@@ -1,8 +1,15 @@
 import articleStyles from '../styles/Article.module.css';
 import Link from 'next/link';
 
-const ArticlesItem = () => {
-  return <Link href=""></Link>;
+const ArticlesItem = ({ article }) => {
+  return (
+    <Link href="/article/[id]" as={`/article/${article.id}`}>
+      <a className={articleStyles.card}>
+        <h3>{article.title} &rarr;</h3>
+        <p>{article.body}</p>
+      </a>
+    </Link>
+  );
 };
 
 export default ArticlesItem;
