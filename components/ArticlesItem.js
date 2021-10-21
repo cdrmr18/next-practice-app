f167bb5e30996b16076631b74235100b17ce407c
Kathy Tavia <kathy.tavia@gmail.com>
2021-10-21 20:46:31 +0200

    set up fetch for articles & added articles comp.

---
+++ components/ArticlesItem.js
@@ -0,0 +1,8 @@
+import articleStyles from '../styles/Article.module.css';
+import Link from 'next/link';
+
+const ArticlesItem = () => {
+  return <Link href=""></Link>;
+};
+
+export default ArticlesItem;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
 import Head from 'next/head';
+import ArticleList from '../components/ArticleList';
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
         <title>Trying my best</title>
         <meta name="keywords" content="web development, programming" />
       </Head>
-      <h1>Practice Next</h1>
+      <ArticleList articles={articles} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/posts?_limit=6'
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
